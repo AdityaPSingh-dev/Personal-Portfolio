94355dfd80b2f49916efed8c368c4ce506c36995
Aditya <adityapsingh559@gmail.com>
2025-04-25 18:17:43 +0530

    changed model

--- app/components/model/model.tsx
+++ app/components/model/model.tsx
@@ -5,18 +5,16 @@ import { Model } from "./Man"; // Assuming Model is a custom 3D model component.
 
 const ModelWithShadows = () => {
   return (
-    <div className="w-[150vh] h-[120vh] md:w-full flex justify-center items-center md:inline-block lg:hidden mt-4 md:mt-0 md:h-[60vh] md:mr-20 overflow-hidden">
+    <div className="w-[150vh] h-[120vh] md:w-full flex justify-center items-center md:inline-block lg:hidden mt-4 md:mt-0 md:h-[60vh] md:mr-20 overflow-hidden touch-[pan-y]">
       <Canvas
-        className=""
-        camera={{ position: [0, 3, 10], fov: 35, near: 0.1, far: 20 }} // Adjust the camera to zoom in closer to the torso
+        className="pointer-events-auto" // keep this if you want interaction
+        camera={{ position: [0, 3, 10], fov: 35, near: 0.1, far: 20 }}
       >
         <ambientLight intensity={0.5} />
         <directionalLight position={[5, 10, 5]} intensity={1} castShadow />
         <Environment preset="sunset" />
         <OrbitControls enableZoom={false} enableRotate={false} />
-        {/* Adjust model's position if needed */}
-        <Model scale={5} position={[0, -7, 0]} />{" "}
-        {/* Adjust position so the upper body is centered */}
+        <Model scale={5} position={[0, -7, 0]} />
       </Canvas>
     </div>
   );
